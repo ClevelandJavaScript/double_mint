d486f562b576ddff8d781c4e97843b7f1eb6ac5d
Dan Shultz <das0118@gmail.com>
2014-05-19 17:48:04 -0400

    define account route

--- app/router.js
+++ app/router.js
@@ -1,7 +1,10 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.resource('accounts');
+  this.resource('accounts', function () {
+    this.resource('account', { path: ':account_id' }, function () {
+    });
+  });
 });
 
 export default Router;
