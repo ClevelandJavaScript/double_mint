b460792a21a043ff36019f9ce7d5beafacd767da
Dan Shultz <das0118@gmail.com>
2014-05-19 17:53:04 -0400

    add format money helper and update template to use it

---
+++ app/helpers/format-money.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Handlebars.makeBoundHelper(function(number) {
+  var ns = number.toString();
+  var result;
+  var decimal = ns.indexOf('.');
+  if (decimal > -1) {
+    if (decimal+2 <= ns.length) { ns = ns + '0'; }
+    result = Array.prototype.slice.call(ns, 0, decimal + 3).join('');
+  } else {
+    result = ns + ".00";
+  }
+  return "$" + result;
+});
