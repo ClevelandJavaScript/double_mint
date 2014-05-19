4c250e7d88f49da95440885679bd11ffa758590c
Dan Shultz <das0118@gmail.com>
2014-05-19 17:50:04 -0400

    add transaction route

--- app/router.js
+++ app/router.js
@@ -3,6 +3,7 @@ var Router = Ember.Router.extend(); // ensure we don't share routes between all 
 Router.map(function() {
   this.resource('accounts', function () {
     this.resource('account', { path: ':account_id' }, function () {
+      this.resource('accounts.transactions', { path: 'transactions'});
     });
   });
 });
---
+++ app/routes/accounts/transactions.js
@@ -0,0 +1,6 @@
+export default Ember.Route.extend({
+  model: function (params) {
+    var account = this.modelFor('account');
+    return this.store.find('transaction', { account_id: account.id });
+  }
+});
