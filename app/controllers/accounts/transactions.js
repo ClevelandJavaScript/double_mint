3bec274811695fd796c168d8f82a94cdcbf8afc6
Dan Shultz <das0118@gmail.com>
2014-05-19 17:57:13 -0400

    support editing item

--- app/controllers/accounts/transactions.js
+++ app/controllers/accounts/transactions.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  itemController: 'accounts/transaction-item'
+  itemController: 'accounts/transaction-item',
+  currentEditingItem: null,
+
+  actions: {
+
+    startEditing: function (item) {
+      var currentItem = this.get('currentEditingItem');
+      if (currentItem !== item) {
+        if (currentItem) { currentItem.set('isEditing', false); }
+        item.set('isEditing', true);
+        this.set('currentEditingItem', item);
+      }
+    }
+
+  }
 });
