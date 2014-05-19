ee15bb01a356c3e49045959f5584d4d0b89afdbf
Dan Shultz <das0118@gmail.com>
2014-05-19 17:57:38 -0400

    support persisting edits

--- app/controllers/accounts/transactions.js
+++ app/controllers/accounts/transactions.js
@@ -1,7 +1,6 @@
 export default Ember.ArrayController.extend({
   itemController: 'Accounts/TransactionItem',
   currentEditingItem: null,
-
   actions: {
 
     startEditing: function (item) {
@@ -11,6 +10,15 @@ export default Ember.ArrayController.extend({
         item.set('isEditing', true);
         this.set('currentEditingItem', item);
       }
+    },
+
+    stopEditing: function () {
+      var currentItem = this.get('currentEditingItem');
+      currentItem.set('isEditing', false);
+      if (currentItem.get('isDirty')) {
+        currentItem.get('content').save();
+      }
+      this.set('currentEditingItem', null);
     }
 
   }
